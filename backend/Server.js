6ef6bbfc90e8c04bbc8b92bb495067a5c935ea5f
Juan David Narvaez <juan_dav.narvaes@uao.edu.co>
2026-09-09 17:12:57 -0500

    arreglo error de conexion

--- backend/Server.js
+++ backend/Server.js
@@ -1,9 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-
+const express = require('express');
 const app = express();
+const port = 3000;
 
-app.use(cors());
+
 app.use(express.json());
 
 app.post("/calcular", (req, res) => {
